cceb46fd33713f821b56c9b3956b87d23e841f62
Shubham-Singh1810 <hittheshubham1810@gmail.com>
2024-12-14 19:45:58 +0530

    after adding socket

--- src/controller/chatController.js
+++ src/controller/chatController.js
@@ -5,24 +5,28 @@ const Chat = require("../model/chat.Schema");
 const chatController = express.Router();
 
 chatController.post("/create-chat", async (req, res) => {
-    try { 
-      const chat = await Chat.create(req.body);
-      sendResponse(res, 200, "Success", {
-        success: true,
-        message: "Message created successfully",
-        data: chat,
-      });
-    } catch (error) {
-      console.log(error);
-      sendResponse(res, 500, "Failed", {
-        message: error.message || "Internal server error",
-      });
-    }
+  try {
+    const chat = await Chat.create(req.body);
+    // Emit the new chat message to all connected clients
+    req.io.emit("new-message", chat); // Broadcast the message
+    sendResponse(res, 200, "Success", {
+      success: true,
+      message: "Message created successfully",
+      data: chat,
+    });
+  } catch (error) {
+    console.log(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+    });
+  }
 });
 
 chatController.post("/get-booking-chat", async (req, res) => {
-  try { 
-    const chat = await Chat.find({bookingId: req.body.bookingId}).sort({ createdAt: 1 });
+  try {
+    const chat = await Chat.find({ bookingId: req.body.bookingId }).sort({ createdAt: 1 });
+    // Emit an event to notify the client of the retrieved chat
+    req.io.emit("chat-history", { bookingId: req.body.bookingId, messages: chat });
     sendResponse(res, 200, "Success", {
       success: true,
       message: "Message retrieved successfully",
